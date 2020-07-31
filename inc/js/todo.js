7730f15ced2d3305f4b92d885495ffa90b6d3b82
cl-song <cl.ssongg@gmail.com>
2020-08-01 00:07:58 +0900

    Refactor: getToDos 중복코드 제거, 주석문구 변경, 코드 정리

--- inc/js/todo.js
+++ inc/js/todo.js
@@ -5,65 +5,58 @@ const toDoListContainer = document.createElement('ul');
 toDoListContainer.className = 'js-toDoList';
 let toDoArr = [];
 
-// 로컬스토리지 저장된 ToDoList 있는지 체크
-function toDoStorageChk() {
-    const toDoList = localStorage.getItem('toDoList');
-    if(toDoList !== null) {
-        getToDoList(toDoList);
+// 로컬스토리지 - 저장된 ToDoList 있는지 체크
+function loadToDos() {
+    const loadedToDos = localStorage.getItem('toDoList');
+    if(loadedToDos !== null) {
+        toDoArr = JSON.parse(loadedToDos);
+        toDoArr.forEach( function(toDoList) {
+            getToDos(toDoList.id, toDoList.text);
+        });
     }
 }
 
-// 이벤트 발생 시 리스트 저장
-function setToDoList() {
+// Input - submit 이벤트 발생 시 리스트 저장
+function saveToDos() {
     toDoForm.addEventListener('submit', function(event) {
         event.preventDefault();
         const currentValue = toDoInput.value;
         if(currentValue !== '') {
             toDoArr.push({
-                text: currentValue,
-                id: `listItem${toDoArr.length + 1}`
+                id: `listItem${toDoArr.length + 1}`,
+                text: currentValue
             });
+            console.log(toDoArr);
             // localStorage.setItem('toDoList', toDoArr);
             localStorage.setItem('toDoList', JSON.stringify(toDoArr));
-            getToDoList(toDoArr);
+            getToDos(toDoArr[toDoArr.length - 1].id, toDoArr[toDoArr.length - 1].text);
             toDoInput.value = '';
         }
     });
 }
 
-// 리스트가 있는 경우
-function getToDoList(toDoList) {
-    if(!toDoArr.length) { // 저장된 리스트가 있는 경우
-        toDoArr = JSON.parse(toDoList);
-        for(let i = 0; i < toDoArr.length; i++) {
-            const loadList = document.createElement('li');
-            loadList.id = toDoArr[i].id;
-            let listTxt = document.createElement('span');
-            listTxt.innerText = toDoArr[i].text;
-            let listBtn = document.createElement('button');
-            listBtn.className = 'btnDelete';
+// 리스트가 있는 경우 화면에 보여줌
+function getToDos(id, text) {
+    const listLi = document.createElement('li');
+    listLi.id = id;
+    const listTxt = document.createElement('span');
+    listTxt.innerText = text;
+    const listBtn = document.createElement('button');
+    listBtn.className = 'btnDelete';
 
-            loadList.appendChild(listTxt);
-            loadList.appendChild(listBtn);
-            toDoListContainer.appendChild(loadList);
-        }
-    } else { // 새로운 리스트 추가하는 경우
-        const addList = document.createElement('li');
-        addList.id = toDoArr[toDoArr.length-1].id;
-        let listTxt = document.createElement('span');
-        listTxt.innerText = toDoArr[toDoArr.length-1].text;
-        let listBtn = document.createElement('button');
-        listBtn.className = 'btnDelete';
-
-        addList.appendChild(listTxt);
-        addList.appendChild(listBtn);
-        toDoListContainer.appendChild(addList);
-    }
+    listLi.appendChild(listTxt);
+    listLi.appendChild(listBtn);
+    toDoListContainer.appendChild(listLi);
     toDoContainer.appendChild(toDoListContainer);
 }
 
+// Btn - click 이벤트 발생 시 리스트 삭제
+function deleteToDos(event) {
+
+}
+
 function init() {
-    toDoStorageChk();
-    setToDoList();
+    loadToDos();
+    saveToDos();
 }
 init();
